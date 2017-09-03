967908b9a7d380874879c141746e2301c68a4674
alexanderKw <alkrivulya@gmail.com>
2017-09-04 01:18:22 +0300

    Low Search

---
+++ dev/js/script.js
@@ -0,0 +1,47 @@
+const endpoint = 'data.json';
+const datas = [];
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => datas.push(...data));
+
+function findMatches(wordToMatch, datas) {
+  return datas.filter(person => {
+    const regex = new RegExp(wordToMatch, 'gi');
+
+    return person.name.match(regex) || person.company.match(regex);
+  });
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, datas);
+
+  const html = matchArray
+    .map(person => {
+      const regex = new RegExp(this.value, 'gi');
+
+      const personName = person.name.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      const personCompany = person.company.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+
+      return `
+        <li>
+          <span class="list-item">${personName}, ${personCompany}</span>
+        </li>
+      `;
+    })
+    .join('');
+
+  list.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const list = document.querySelector('.list');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
